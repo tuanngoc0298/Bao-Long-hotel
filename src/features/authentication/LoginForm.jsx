fa9eaab3907629c12291da6bf1e11165da264ef1
tuanngoc0298 <tuanngoc0298@gmail.com>
2023-12-29 20:35:29 +0700

    Complete

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("test@gmail.com");
+  const [password, setPassword] = useState("tuanngoc0298");
   const { isLoading, login } = useLogin();
   function handleSubmit(e) {
     e.preventDefault();
